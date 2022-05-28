6091ca4dbeec9ffefa06062a0957940b8eee71d2
Wisdom woke <99021060+WISDOM-WOKE22@users.noreply.github.com>
2022-05-28 23:29:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function writeText() {
 setInterval(writeText,200);
 
 window.addEventListener("load",() => {
-//     preloader.style.display="none";
+    preloader.style.display="none";
 });
 
 menuBar.addEventListener("click", () => {
